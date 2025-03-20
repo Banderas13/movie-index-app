79b3d529fff3e894478c178b668e51dda38fcff8
Julio Raynier <raynier97@hotmail.com>
2025-03-20 12:45:32 +0100

    Fresh start & added tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-const Card = ({ title }) => {
-  const [hasLiked, setHasLiked] = useState(false);
-  return (
-    <div className='card'>
-      <h2>{title}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked ? '💜' : '🤍'}
-      </button>
-    </div>
-  )
-}
+import React from 'react'
 
 const App = () => {
   return (
-    <div className='card-container'>
-      <Card title="Star Wars" rating={5} isCool={true}/>
-      <Card title="The Matrix" />
-      <Card title="The Dark Knight" />
-    </div>
+    <div>App</div>
   )
 }
 
