5235cf88a607f0b0782c378cafec754953690563
Julio Raynier <raynier97@hotmail.com>
2025-03-23 17:25:34 +0100

    Refactor App component to manage loading state and display movie list. Enhanced error handling and updated UI to show loading indicator and movie titles.

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,14 @@ const API_OPTIONS = {
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('')
-
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+
     try {
       const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
 
@@ -30,11 +34,17 @@ const App = () => {
       
       if(data.Response === 'False') {
         setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
       }
+
+      setMovieList(data.results || []);
 
     } catch (error) {
       console.error(`Error catching movies: ${error}`);
       setErrorMessage(`Error fetching movies. Please try again later.`)
+    } finally {
+      setIsLoading(false);
     }
   }
 
@@ -55,7 +65,17 @@ const App = () => {
         <section className='all-movies'>
           <h2>All Movies</h2>
 
-          {errorMessage && <p className='text-red-500'>{errorMessage}</p>}
+          {isLoading ? (
+            <p className='text-white'>Loading ...</p>
+          ) : errorMessage ? (
+            <p className='text-red-500'>{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <p className='text-white'>{movie.title}</p>
+              ))}
+            </ul>
+          )}
         </section>
         
       </div>
